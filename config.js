9b3d0253d120a799edb7149c5e86b25fc206a192
Hemis <[emailprotected]>
2026-03-28 21:40:34 -0400

    3/28 supabase v2

--- config.js
+++ config.js
@@ -7,4 +7,4 @@
 const SUPABASE_URL = "https://pofmhobxednullgxtoah.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvZm1ob2J4ZWRudWxsZ3h0b2FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3NDQ1ODUsImV4cCI6MjA5MDMyMDU4NX0.UkQo6Kpxw_ZFBp9SYvSGYoYj3XBuqj9nZTyI4Gf5kV4";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const dbClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
